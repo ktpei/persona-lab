93f0f6dfe844cafb0f79cf146683f1bf82342ffd
William Abraham <williamjohnabraham1@gmail.com>
2026-02-24 14:46:35 -0500

    lol

--- apps/worker/browser-entrypoint.sh
+++ apps/worker/browser-entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -e
 
-# Start socat to proxy 0.0.0.0:9222 → 127.0.0.1:9223 (where Chromium will listen)
-socat TCP-LISTEN:9222,fork,reuseaddr,bind=0.0.0.0 TCP:127.0.0.1:9223 &
-
-# Launch Chromium with CDP on localhost:9223
 exec /ms-playwright/chromium-*/chrome-linux64/chrome \
   --headless=new \
   --no-sandbox \
   --disable-gpu \
   --disable-dev-shm-usage \
-  --remote-debugging-address=127.0.0.1 \
-  --remote-debugging-port=9223 \
+  --remote-debugging-address=0.0.0.0 \
+  --remote-debugging-port=9222 \
   --remote-allow-origins=* \
   --disable-background-networking \
   --disable-default-apps \
